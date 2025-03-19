d4651f218754251c093f52167b7a29a88c1515ce
james.norman@proteusllp.com <james.norman@proteusllp.com>
2025-03-19 10:12:53 +0000

    simplified catastrophe example

--- example_catastrophes.py
+++ example_catastrophes.py
@@ -1,36 +1,38 @@
 from pcm import config, distributions
 from pcm.config import xp as np
-from pcm.variables import ProteusVariable
-from pcm.catastrophes import SimEventId, SimEventLossTable
+from pcm.variables import ProteusVariable, FreqSevSims, StochasticScalar
 import pandas as pd  # noqa
+import math
 
-config.n_sims = 100000
-
-yet_df = pd.read_csv("data/master_ylt.csv")
-master_yet = SimEventId(
-    yet_df["SimNo"].values, yet_df["SimEventId"].values, n_sims=config.n_sims
-)
-ylt = SimEventLossTable.from_dataframe(pd.read_csv("data/cat_ylt.csv"), master_yet)
-ylt2 = SimEventLossTable.from_dataframe(pd.read_csv("data/cat_ylt2.csv"), master_yet)
-
-ylts = ProteusVariable("class", values={"Property": ylt, "Reinsurance": ylt2})
-total_ylt = sum(ylts)
-inflation_rate = ProteusVariable(
-    dim_name="year",
-    values={
-        str(2025 + year): distributions.Normal(0.05, 0.02).generate()
-        for year in range(10)
+n_sims = 100000
+config.n_sims = n_sims
+lobs = ["Motor", "Property", "Liability", "Marine", "Aviation"]
+# load the cat ylts
+df = pd.read_csv("data/cat_ylt.csv", index_col=0)
+# upsample the cat ylts to the correct number of simulations
+ylt_sims = 10000
+up_sample_factor = math.ceil(n_sims / ylt_sims)
+sim_index = df["sim"].values.repeat(up_sample_factor)
+cat_losses = ProteusVariable(
+    "lob",
+    {
+        lob: FreqSevSims(
+            sim_index,
+            df[lob].values.repeat(up_sample_factor),
+            n_sims=config.n_sims,
+        )
+        for lob in lobs
     },
 )
-cumulative_inflation_index = np.cumprod(1 + inflation_rate)
 
-scaled_ylt = total_ylt * cumulative_inflation_index
+inflation_rate = distributions.Normal(0.05, 0.02).generate()
 
-y: ProteusVariable = np.minimum(np.maximum(scaled_ylt - 1000000, 0), 100000)
+scaled_cat_losses_by_lob = cat_losses * (1 + inflation_rate)
 
-z = ProteusVariable(
-    dim_name="year",
-    values={yr: y[yr].aggregate() for yr in y.values.keys()},
+scaled_cat_losses = sum(scaled_cat_losses_by_lob)
+
+recoveries: FreqSevSims = np.minimum(
+    np.maximum(scaled_cat_losses - 10000000, 0), 10000000
 )
 
-z.show_cdf()
+recoveries.aggregate().show_cdf()
