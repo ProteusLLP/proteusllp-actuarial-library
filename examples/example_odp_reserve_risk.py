a2cabf0434043fb0b0bf685a738b31b72ad4b1a2
James Norman <134841919+pythactuary@users.noreply.github.com>
2026-08-26 19:58:42 +0100

    Apply Ruff formatting to ODP example

--- examples/example_odp_reserve_risk.py
+++ examples/example_odp_reserve_risk.py
@@ -113,8 +113,8 @@ class ODPModel:
         self.estimate_phi()
         self.build_posterior()
         phi = self.phi
-        total_by_origin: variables.ProteusVariable[stochastic_scalar.StochasticScalar] = (
-            variables.ProteusVariable("op", {})
+        total_by_origin: variables.ProteusVariable[stochastic_scalar.StochasticScalar] = variables.ProteusVariable(
+            "op", {}
         )
 
         for op in self.origin_periods:
