902b13b636cd96d5693a10c42c4765dee2bc7797
James Norman <134841919+pythactuary@users.noreply.github.com>
2026-08-26 19:17:13 +0100

    Use module namespaces in XoL tower example

--- examples/example_xol_tower.py
+++ examples/example_xol_tower.py
@@ -2,18 +2,17 @@
 
 import numpy as np
 
-from pal import XoLTower, config, distributions
-from pal.frequency_severity import FreqSevSims, FrequencySeverityModel
+from pal import config, contracts, distributions, frequency_severity, variables
 
 config.n_sims = 100_000
 
 sev_dist = distributions.GPD(shape=0.33, scale=100_000, loc=1_000_000)
 freq_dist = distributions.Poisson(mean=2)
 
-losses_pre_cap = FrequencySeverityModel(freq_dist, sev_dist).generate()
+losses_pre_cap = frequency_severity.FrequencySeverityModel(freq_dist, sev_dist).generate()
 policy_limit = 5_000_000
 # you can apply standard numpy ufuncs to the losses
-losses_post_cap: FreqSevSims = np.minimum(losses_pre_cap, policy_limit)  # type: ignore[misc]
+losses_post_cap: variables.FreqSevSims = np.minimum(losses_pre_cap, policy_limit)  # type: ignore[misc]
 
 # you can apply standard numerical operations to the losses
 losses_with_lae = losses_post_cap * 1.05
@@ -22,7 +21,7 @@ stochastic_inflation = distributions.Normal(0.05, 0.02).generate()
 # you can multiply frequency severity losses with other standard simulations
 gross_losses = losses_with_lae * (1 + stochastic_inflation)
 
-prog = XoLTower(
+prog = contracts.XoLTower(
     limit=[1_000_000, 1_000_000, 1_000_000, 1_000_000, 10_000_000],
     excess=[1_000_000, 2_000_000, 3_000_000, 4_000_000, 5_000_000],
     aggregate_limit=[3_000_000, 2_000_000, 1_000_000, 1_000_000, 10_000_000],
