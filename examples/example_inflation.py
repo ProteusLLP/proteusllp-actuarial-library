efa772632e1a20160b28399737c0635d5a9c9cf4
James Norman <134841919+pythactuary@users.noreply.github.com>
2026-08-26 19:16:50 +0100

    Use variables namespace in inflation example

--- examples/example_inflation.py
+++ examples/example_inflation.py
@@ -1,12 +1,11 @@
 """An example of loading an inflation series from the Proteus Scenario Generator."""
 
-from pal import config
-from pal.variables import ProteusVariable
+from pal import config, variables
 
 n_sims = 100000
 config.n_sims = n_sims
 
-inflation_index = ProteusVariable.from_csv(
+inflation_index = variables.ProteusVariable.from_csv(
     "data/proteus_scenario_generator/Economics_USD_Inflation_Inflation Index.csv",
     "Time",
     "Index",
