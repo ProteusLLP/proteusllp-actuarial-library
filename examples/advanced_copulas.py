17f3520fb1dd41f6acd3a76e1c91a5668b3451ea
James Norman <134841919+pythactuary@users.noreply.github.com>
2026-08-26 21:43:19 +0100

    Use direct PAL submodule imports

--- examples/advanced_copulas.py
+++ examples/advanced_copulas.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-from pal import copulas, distributions, variables
+from pal.copulas import HuslerReissCopula
+from pal.distributions import Gamma
+from pal.variables import ProteusVariable
 
 lobs = ["Motor", "Property", "Liability", "Specialty"]
-losses = variables.ProteusVariable(
+losses = ProteusVariable(
     "lob",
     {
-        lob: distributions.Gamma(alpha=alpha, theta=theta).generate()
+        lob: Gamma(alpha=alpha, theta=theta).generate()
         for lob, alpha, theta in zip(lobs, [2.0, 1.0, 4.0, 0.5], [0.5, 0.75, 0.25, 2])
     },
 )
 tail_dependence_matrix = np.array([[1, 0.5, 0.2, 0.1], [0.5, 1, 0.3, 0.2], [0.2, 0.3, 1, 0.4], [0.1, 0.2, 0.4, 1]])
-copulas.HuslerReissCopula.from_tail_dependence_matrix(tail_dependence_matrix).apply(losses)
+HuslerReissCopula.from_tail_dependence_matrix(tail_dependence_matrix).apply(losses)
 losses.rank_scatter_plot(title="Husler-Reiss Copula").show()
