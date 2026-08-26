d3a8eccaa58bba48e326e347a8559af0997c0cee
James Norman <134841919+pythactuary@users.noreply.github.com>
2026-08-26 19:16:30 +0100

    Use module namespaces in capital allocation example

--- examples/capital_allocation_example.py
+++ examples/capital_allocation_example.py
@@ -4,17 +4,16 @@ The example is taken from the paper 'Capital Allocation by Percentile Layer', by
  Casualty Acturaial Society Volume 3 issue 1
 """
 
-from pal import ProteusVariable, risk_measures, set_default_n_sims, set_random_seed
-from pal.distributions import Bernoulli, Exponential
+from pal import distributions, risk_measures, set_default_n_sims, set_random_seed, variables
 
 set_random_seed(42)
 set_default_n_sims(100_000)
-losses = ProteusVariable(
+losses = variables.ProteusVariable(
     "lob",
     {
-        "Fire": Bernoulli(0.25).generate() * Exponential(4e6).generate(),
-        "Wind": Bernoulli(0.05).generate() * Exponential(20e6).generate(),
-        "EQ": Bernoulli(0.01).generate() * Exponential(100e6).generate(),
+        "Fire": distributions.Bernoulli(0.25).generate() * distributions.Exponential(4e6).generate(),
+        "Wind": distributions.Bernoulli(0.05).generate() * distributions.Exponential(20e6).generate(),
+        "EQ": distributions.Bernoulli(0.01).generate() * distributions.Exponential(100e6).generate(),
     },
 )
 
